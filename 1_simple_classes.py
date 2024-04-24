92858c7427346a41244792908ddccc097c322d9e
Ilya <karpov.ilja@gmail.com>
2024-04-24 10:24:08 +0300

    2 tasks

---
+++ 1_simple_classes.py
@@ -0,0 +1,20 @@
+#
+# Dokumentācija: https://www.w3schools.com/python/python_classes.asp
+# 
+# 1. Izveido klasi Shape:
+# ar atribūtem - garums un platums
+# metodem kuras skaita - perimetra garumu un laukuma platību
+# Kad klase ir izveidota - izveidot divus Shape objektus un notestēt abas metodes
+# 
+# 2. Izveido klasi BankAccount:
+# ar atribūtem - konta numuru, atlikumu un konta turētāja vārdu
+# metodem - papildināt bilanci, samazināt bilanci, izdrukāt konta informāciju (visus atribūtus)
+# Kad klase ir izveidota - izveidot vienu BankAccount objektu un notestēt metodes
+# 
+# 3. Izveido klasi Product:
+# 
+# ar atribūtem - nosaukums, cena, daudzums
+# metodem - mainīt cenu, mainīt daudzumu, saskaitīt kopējo summu (cena * daudzums), 
+# Kad klase ir izveidota - izveidot vienu Product objektu un notestēt metodes
+# 
+
---
+++ 2_inheritance_classes.py
@@ -0,0 +1,15 @@
+#
+# Dokumentācija: 
+# - https://www.w3schools.com/python/python_classes.asp
+# - https://www.w3schools.com/python/python_inheritance.asp
+# - https://www.w3schools.com/python/python_polymorphism.asp
+# 
+# Uzdevums: Izveido kases aparāta čeku (klase Check)
+# 
+# Funkcionalitāte:
+# - pievienot preci (nosaukums, cena)
+# - ievadīt atlaidi
+# - dzest preci pēc indeksa
+# - saskaitīt kopēju summu (summēt visas preces un saskaitīt atlaidi)
+# - izdrukāt čeku (visas preces un galējo summu)
+#
